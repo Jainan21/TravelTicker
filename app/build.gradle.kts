8ecc1d9e4d19073ba6ed36a65d2434d0bae0d958
Jainan21 <143272157+Jainan21@users.noreply.github.com>
2024-11-14 11:19:39 +0700

    connect db
    u yeah

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
+    id("com.google.gms.google-services")
 }
-
 android {
     namespace = "com.example.travelticker"
     compileSdk = 34
@@ -40,4 +40,7 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+
+    implementation(platform("com.google.firebase:firebase-bom:33.5.1"))
+    implementation("com.google.firebase:firebase-database")
 }
