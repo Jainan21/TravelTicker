cde6b3336396c7214ff942d2b59e132cbc0aa5c1
anhhong905 <nguyenanhhong905@gmail.com>
2024-11-28 22:42:38 +0700

    ah
    commit

--- app/src/main/java/com/example/travelticker/TrangChu.java
+++ app/src/main/java/com/example/travelticker/TrangChu.java
@@ -1,19 +1,11 @@
 package com.example.travelticker;
 
 import android.os.Bundle;
-import android.os.Handler;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
-
-import com.example.travelticker.Fragment.FragmentOnboarding1;
-import com.example.travelticker.Fragment.FragmentOnboarding2;
-import com.example.travelticker.Fragment.FragmentOnboarding3;
 
 public class TrangChu extends AppCompatActivity {
-    private Handler handler;
-    private Runnable runnable;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,39 +13,5 @@ public class TrangChu extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_trang_chu);
 
-        //Hiển thị fragment đầu tiên
-        getSupportFragmentManager().beginTransaction()
-                .replace(R.id.fragment_container, new FragmentOnboarding1())
-                .commit();
-
-        //Tự động chuyển Fragment
-        handler = new Handler();
-        runnable = new Runnable() {
-            @Override
-            public void run() {
-                Fragment currentFragment = getSupportFragmentManager().findFragmentById(R.id.fragment_container);
-                if(currentFragment instanceof FragmentOnboarding1){
-
-                    //Chuyển sang Onboarding2
-                    getSupportFragmentManager().beginTransaction()
-                            .replace(R.id.fragment_container, new FragmentOnboarding2())
-                            .commit();
-                } else if (currentFragment instanceof FragmentOnboarding2) {
-                    //Chuyển sang Onboarding 3
-                    getSupportFragmentManager().beginTransaction()
-                            .replace(R.id.fragment_container, new FragmentOnboarding3())
-                            .commit();
-
-                }
-            }
-        };
-
-        handler.postDelayed(runnable, 2000);
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        handler.removeCallbacks(runnable);
     }
 }
