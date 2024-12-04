e2c25374c3e2caf7174497e68ea757b814d05623
anhphi1125 <166363173+anhphi1125@users.noreply.github.com>
2024-12-04 12:02:53 +0700

    search

---
+++ app/src/main/java/com/example/travelticker/Adapter/SearchAdapter.java
@@ -0,0 +1,65 @@
+package com.example.travelticker.Adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.example.travelticker.Model.Post;
+import com.example.travelticker.R;
+
+import java.util.ArrayList;
+
+public class SearchAdapter extends RecyclerView.Adapter<SearchAdapter.SearchViewHolder>{
+
+    ArrayList<Post> list;
+    Context context;
+
+    public SearchAdapter(ArrayList<Post> list, Context context) {
+        this.list = list;
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public SearchViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.sanpham_view_holder, parent, false);
+        return new SearchViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull SearchViewHolder holder, int position) {
+        Post item = list.get(position);
+
+        Glide.with(context).load(item.getImg()).into(holder.imgPost);
+        holder.txtTomTat.setText(item.getNoiDung());
+        holder.txtTieuDe.setText(item.getTieuDe());
+    }
+
+    @Override
+    public int getItemCount() {
+        return list.size();
+    }
+
+    public class SearchViewHolder extends RecyclerView.ViewHolder {
+        public TextView txtTieuDe, txtTomTat, txtDiaChi;
+        public ImageView imgPost;
+        public Button btnHasFavo;
+        public SearchViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            this.btnHasFavo = itemView.findViewById(R.id.btnHasFavo);
+            this.imgPost = itemView.findViewById(R.id.imgPost);
+            this.txtTieuDe = itemView.findViewById(R.id.txtTieuDe);
+            this.txtDiaChi = itemView.findViewById(R.id.txtDiaChi);
+            this.txtTomTat = itemView.findViewById(R.id.txtTomTat);
+        }
+    }
+}
---
+++ app/src/main/java/com/example/travelticker/Search.java
@@ -0,0 +1,139 @@
+package com.example.travelticker;
+
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.travelticker.Adapter.SearchAdapter;
+import com.example.travelticker.Model.Post;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+
+public class Search extends AppCompatActivity {
+
+    Toolbar tbSearch;
+    EditText edtSearch;
+    Button btnSearch;
+    RecyclerView rcvSearch;
+
+    ArrayList<Post> listPost;
+    ArrayList<Post> listSearch;
+
+    SearchAdapter adapter;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.search_fragment);
+
+        tbSearch = findViewById(R.id.tbSearch);
+        edtSearch = findViewById(R.id.edtSearch);
+        btnSearch = findViewById(R.id.btnSearch);
+        rcvSearch = findViewById(R.id.rcvSearch);
+
+        listPost = new ArrayList<>();
+
+        rcvSearch.setLayoutManager(new LinearLayoutManager(this));
+
+        setSupportActionBar(tbSearch);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setTitle("Tìm kiếm");
+
+        getListPost();
+
+        edtSearch.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                // lọc
+                String query = s.toString().toLowerCase().trim();
+                if (query != null && !query.isEmpty()) {
+                    findListSearch(query);
+                } else {
+                    // Nếu không có từ khóa tìm kiếm, hiển thị tất cả bài đăng
+                    findListSearch("");
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+            }
+        });
+    }
+
+    public void getListPost() {
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference databaseRef = database.getReference("BaiDang");
+
+        // Lắng nghe sự thay đổi dữ liệu trong Firebase
+        databaseRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for (DataSnapshot snapshot1 : snapshot.getChildren()) {
+                    Post post = snapshot1.getValue(Post.class);
+
+                    if (post != null) {
+                        listPost.add(post);
+                    } else {
+                        Toast.makeText(Search.this, "Lỗi lưu dữ liệu !!!", Toast.LENGTH_SHORT).show();
+                    }
+                }
+
+//                // Cập nhật adapter khi dữ liệu đã có
+//                adapter = new SearchAdapter(listPost, Search.this);
+//                rcvSearch.setAdapter(adapter);
+                Toast.makeText(Search.this, "Dữ liệu đã được tải thành công", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                // Lỗi khi đọc dữ liệu
+                Toast.makeText(Search.this, "Lỗi đọc dữ liệu", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    public void findListSearch(String query) {
+        // Tìm kiếm các bài đăng theo từ khóa
+        if (listSearch != null) {
+            listSearch.clear();
+        } else {
+            listSearch = new ArrayList<>();
+        }
+
+        for (Post post : listPost) {
+            if (post.getTieuDe().toLowerCase().contains(query)) {
+                listSearch.add(post);
+            }
+        }
+
+        // Cập nhật lại RecyclerView
+        if (!listSearch.isEmpty()) {
+            adapter = new SearchAdapter(listSearch, this);
+            rcvSearch.setAdapter(adapter);
+        } else {
+            Toast.makeText(this, "Không tìm thấy kết quả", Toast.LENGTH_SHORT).show();
+        }
+    }
+}
